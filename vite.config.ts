e52768da3155bb3c40f21229cd1645a4e7c3105b
gorushkin <gorushkin@users.noreply.github.com>
2026-09-20 20:38:45 +0300

    refactor: simplify App component and integrate new Button component
    - Removed unnecessary imports and code from App.tsx, replacing it with a simple Button component.
    - Added a new Button component in src/components/ui/button.tsx with customizable variants and sizes.
    - Updated index.css to include Tailwind CSS and custom styles, removing old CSS variables.
    - Created a utility function for class name management in src/lib/utils.ts.
    - Adjusted TypeScript configuration to set baseUrl and ignore deprecations.
    - Modified Vite configuration to include Tailwind CSS plugin and updated path resolution for aliases.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
+// import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+  // plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
 })
